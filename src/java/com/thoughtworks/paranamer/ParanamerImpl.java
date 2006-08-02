338f35f6db3f3d0599efb9960a6f52e03e2e62cc
paul <paul@75d258eb-f418-0410-b93b-c6a5b200b6a1>
2006-08-02 05:48:20 +0000

    some constants
    git-svn-id: https://svn.codehaus.org/paranamer/trunk@55 75d258eb-f418-0410-b93b-c6a5b200b6a1

--- src/java/com/thoughtworks/paranamer/ParanamerImpl.java
+++ src/java/com/thoughtworks/paranamer/ParanamerImpl.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParanamerImpl implements Paranamer {
+    private static final String SPACE = " ";
+    private static final String NEWLINE = "\n";
 
     /**
      * Lookup a method, and return null if its not there
@@ -21,9 +23,9 @@ public class ParanamerImpl implements Paranamer {
      */
     public Method lookupMethod(ClassLoader classLoader, String className, String methodName, String paramNames) {
         String mappings = getMappingsFromResource(classLoader.getResourceAsStream("META-INF/ParameterNames.txt"));
-        StringBuffer classAndMethodAndParamNamesSB = new StringBuffer("\n").append(className).append(" ").append(methodName);
+        StringBuffer classAndMethodAndParamNamesSB = new StringBuffer(NEWLINE).append(className).append(SPACE).append(methodName);
         if (!paramNames.equals("")) {
-            classAndMethodAndParamNamesSB.append(" ").append(paramNames);
+            classAndMethodAndParamNamesSB.append(SPACE).append(paramNames);
         }
         String classAndMethodAndParamNames = classAndMethodAndParamNamesSB.toString();
 
@@ -61,7 +63,7 @@ public class ParanamerImpl implements Paranamer {
             LineNumberReader lineReader = new LineNumberReader(inputStreamReader);
             String line = readLine(lineReader);
             while (line != null) {
-                paramMappingsBuffer.append(line).append("\n");
+                paramMappingsBuffer.append(line).append(NEWLINE);
                 line = readLine(lineReader);
             }
             return paramMappingsBuffer.toString();
@@ -88,11 +90,11 @@ public class ParanamerImpl implements Paranamer {
         if (mappings == null) {
             return new String[0];
         }
-        String classAndMethodName = "\n" + className + " " + methodName + " ";
+        String classAndMethodName = NEWLINE + className + SPACE + methodName + SPACE;
         int ix = mappings.indexOf(classAndMethodName);
         List matches = new ArrayList();
         while (ix > 0) {
-            matches.add(mappings.substring(ix + classAndMethodName.length(), mappings.indexOf(" ", ix + classAndMethodName.length() + 1)).trim());
+            matches.add(mappings.substring(ix + classAndMethodName.length(), mappings.indexOf(SPACE, ix + classAndMethodName.length() + 1)).trim());
             ix = mappings.indexOf(classAndMethodName, ix + 1);
         }
         return (String[]) matches.toArray(new String[matches.size()]);
@@ -100,7 +102,7 @@ public class ParanamerImpl implements Paranamer {
 
     public Constructor lookupConstructor(ClassLoader classLoader, String className, String paramNames) {
         String mappings = getMappingsFromResource(classLoader.getResourceAsStream("META-INF/ParameterNames.txt"));
-        String classAndConstructorAndParamNames = "\n" + className + " " + className.substring(className.lastIndexOf(".") + 1) + " " + paramNames + " ";
+        String classAndConstructorAndParamNames = NEWLINE + className + SPACE + className.substring(className.lastIndexOf(".") + 1) + SPACE + paramNames + SPACE;
         int ix = mappings.indexOf(classAndConstructorAndParamNames);
         if (ix != -1) {
             String methodParamTypes = extractParamerTypesFromFoundMethod(ix, classAndConstructorAndParamNames, mappings);
@@ -125,7 +127,7 @@ public class ParanamerImpl implements Paranamer {
 
     private String extractParamerTypesFromFoundMethod(int ix, String classAndConstructorAndParamNames, String mappings) {
         int start = ix + classAndConstructorAndParamNames.length();
-        int end = mappings.indexOf("\n", start + 1);
+        int end = mappings.indexOf(NEWLINE, start + 1);
         String methodParamTypes = mappings.substring(start, end).trim();
         return methodParamTypes;
     }
