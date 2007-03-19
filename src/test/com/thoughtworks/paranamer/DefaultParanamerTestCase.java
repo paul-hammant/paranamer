eae6a52c0eaac93ba62b4feb77e8a357fd72f396
paul <paul@75d258eb-f418-0410-b93b-c6a5b200b6a1>
2007-03-19 13:33:33 +0000

    work on asm and state checking
    git-svn-id: https://svn.codehaus.org/paranamer/trunk@89 75d258eb-f418-0410-b93b-c6a5b200b6a1

--- src/test/com/thoughtworks/paranamer/DefaultParanamerTestCase.java
+++ src/test/com/thoughtworks/paranamer/DefaultParanamerTestCase.java
@@ -1,90 +1,9 @@
 package com.thoughtworks.paranamer;
 
-import junit.framework.TestCase;
-
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.NoSuchElementException;
-
-public class DefaultParanamerTestCase extends TestCase {
-    private DefaultParanamer paranamer;
+public class DefaultParanamerTestCase extends AbstractParanamerTestCase {
     
     protected void setUp() throws Exception {
         paranamer = new DefaultParanamer();
-    }
-
-    public void testLookupMethodReturnsNullIfMethodNotFound()
-            throws IOException {
-        Object method = paranamer.lookupMethod(
-                Paranamer.class.getClassLoader(),
-                "com.thoughtworks.paranamer.QdoxParanamerGenerator",
-                "generate", "hello,goodbye");
-        assertNull(method);
-    }
-
-    public void testLookupMethodReturnsNullIfClassNotFound()
-            throws IOException {
-        Object method = paranamer.lookupMethod(
-                Paranamer.class.getClassLoader(), "paranamer.Footle",
-                "generate", "sourcePath,rootPackage");
-        assertNull(method);
-    }
-
-    public void testLookupParanamerCanIndicateThatUnableToGetParameterNames()
-            throws IOException {
-        ClassLoader cl = new ClassLoader(){};
-        Object method = paranamer.lookupMethod(cl, "Blah", "doBlah", "blah");
-        assertNull(method);
-        int x = paranamer.isParameterNameDataAvailable(cl,"Blah", "doBlah");
-        assertEquals(Paranamer.NO_PARAMETER_NAMES_LIST, x);
-
-    }
-
-
-    public void testLookupMethodEndsWithUnknownClass() throws IOException {
-        Object method = paranamer.lookupMethod(
-                Paranamer.class.getClassLoader(), "foo.Bar", "generate",
-                "hello,goodbye");
-        assertNull(method);
-    }
-
-    public void testLookupFailsIfResourceMissing() throws IOException {
-        Paranamer paranamer = new DefaultParanamer("/inexistent/resource");
-        Method m = paranamer.lookupMethod(Paranamer.class.getClassLoader(),
-                "com.thoughtworks.paranamer.QdoxParanamerGenerator",
-                "generate", "sourcePath,rootPackage");
-        assertTrue("null expected", m == null);
-    }
-
-    public void testMethodWithNoArgsCanBeRetrievedByParameterNames()
-            throws IOException, NoSuchMethodException {
-        Method method = paranamer.lookupMethod(
-                Paranamer.class.getClassLoader(),
-                "com.thoughtworks.paranamer.ParanamerGeneratorMojo", "execute", "");
-        assertEquals(ParanamerGeneratorMojo.class.getMethod("execute", new Class[0]),
-                method);
-    }
-
-    public void testMethodWithNoArgsCanBeRetrievedAndShowNoParameterNames()
-            throws IOException, NoSuchMethodException {
-        String[] choices = paranamer.lookupParameterNames(Paranamer.class
-                .getClassLoader(), "com.thoughtworks.paranamer.ParanamerGeneratorMojo",
-                "execute");
-        assertEquals(0, choices.length);
-    }
-
-    public void testLookupParameterNamesForMethod() throws Exception {
-        Method method = QdoxParanamerGenerator.class.getMethod("write", new Class[] {String.class, String.class});
-        String parameters = paranamer.lookupParameterNamesForMethod(method);
-
-        assertEquals("outputPath,content", parameters);
-    }
-
-    public void testLookupParameterNamesForMethodWhenNoArg() throws Exception {
-        Method method = DefaultParanamer.class.getMethod("toString", new Class[0]);
-        String parameters = paranamer.lookupParameterNamesForMethod(method);
-
-        assertEquals("", parameters);
     }
 
 }
