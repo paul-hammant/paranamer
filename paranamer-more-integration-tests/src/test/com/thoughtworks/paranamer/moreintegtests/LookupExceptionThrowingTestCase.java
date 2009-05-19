3568363ef43ff5ab6dbe8604a3f09c612e0c7316
paul <paul@75d258eb-f418-0410-b93b-c6a5b200b6a1>
2009-05-19 11:05:08 +0000

    add testing of conditional exception
    git-svn-id: https://svn.codehaus.org/paranamer/trunk@296 75d258eb-f418-0410-b93b-c6a5b200b6a1

--- paranamer-more-integration-tests/src/test/com/thoughtworks/paranamer/moreintegtests/LookupExceptionThrowingTestCase.java
+++ paranamer-more-integration-tests/src/test/com/thoughtworks/paranamer/moreintegtests/LookupExceptionThrowingTestCase.java
@@ -29,42 +29,39 @@
  */
 package com.thoughtworks.paranamer.moreintegtests;
 
-import junit.framework.TestCase;
-import com.thoughtworks.paranamer.CachingParanamer;
-import com.thoughtworks.paranamer.AdaptiveParanamer;
-import com.thoughtworks.paranamer.Paranamer;
 import com.thoughtworks.paranamer.BytecodeReadingParanamer;
+import com.thoughtworks.paranamer.Paranamer;
+import com.thoughtworks.paranamer.ParameterNamesNotFoundException;
 
 import java.lang.reflect.Method;
+
+import junit.framework.TestCase;
 
 /**
  * This test has to be run with -g:none for Javac.
  * Thus, use the Maven build for this rather than in IDEA/Ecplise
  */
-public class Paranamer0009TestCase extends TestCase {
-
+public class LookupExceptionThrowingTestCase extends TestCase {
+    
     public void methodToFind(String name) {
 		assert name != null;
 	}
                  
 	// fails when compiled with {{-g:none}}, works with {{-g}}. Should skip the lookup with {{-g:none}}.
-	public void testParanamer() throws Exception {
+	public void testCanControlThrowingOfException() throws Exception {
 		BytecodeReadingParanamer paranamer = new BytecodeReadingParanamer();
 
 		Method method = getClass().getMethod("methodToFind", new Class[] { String.class });
 
-        assertEquals (Paranamer.NO_PARAMETER_NAMES_FOR_CLASS_AND_MEMBER, paranamer.areParameterNamesAvailable(
-				Paranamer0009TestCase.class, method.getName()));
-	}
+        try {
+            String[] strings = paranamer.lookupParameterNames(method);
+            fail("should have barfed");
+        } catch (ParameterNamesNotFoundException e) {
+            // expected
+        }
+        String[] strings = paranamer.lookupParameterNames(method, false);
+        assertEquals(0, strings.length);
+    }
 
-	// fails when compiled with {{-g:none}}, works with {{-g}}. Should skip the lookup with {{-g:none}}.
-	public void testParanamer2() throws Exception {
-		CachingParanamer paranamer = new CachingParanamer(new AdaptiveParanamer());
-
-		Method method = getClass().getMethod("methodToFind", new Class[] { String.class });
-
-        assertEquals (Paranamer.NO_PARAMETER_NAMES_FOR_CLASS_AND_MEMBER, paranamer.areParameterNamesAvailable(
-				Paranamer0009TestCase.class, method.getName()));
-	}
 
 }
