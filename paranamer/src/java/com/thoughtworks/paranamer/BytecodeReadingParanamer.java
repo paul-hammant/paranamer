c34927f5dcaa48226a06771768fa2fc03b5be008
Paul Hammant <paul@hammant.org>
2012-01-25 18:20:39 -0600

    PARANAMER-20 - patch applied to close inut streams in all cases

--- paranamer/src/java/com/thoughtworks/paranamer/BytecodeReadingParanamer.java
+++ paranamer/src/java/com/thoughtworks/paranamer/BytecodeReadingParanamer.java
@@ -503,29 +503,37 @@ public class BytecodeReadingParanamer implements Paranamer {
             if (is == null) {
                 throw new IOException("Class not found");
             }
-            byte[] b = new byte[is.available()];
-            int len = 0;
-            while (true) {
-                int n = is.read(b, len, b.length - len);
-                if (n == -1) {
-                    if (len < b.length) {
-                        byte[] c = new byte[len];
-                        System.arraycopy(b, 0, c, 0, len);
-                        b = c;
-                    }
-                    return b;
-                }
-                len += n;
-                if (len == b.length) {
-                    int last = is.read();
-                    if (last < 0) {
-                        return b;
-                    }
-                    byte[] c = new byte[b.length + 1000];
-                    System.arraycopy(b, 0, c, 0, len);
-                    c[len++] = (byte) last;
-                    b = c;
-                }
+            try {
+		          byte[] b = new byte[is.available()];
+		          int len = 0;
+		          while (true) {
+		              int n = is.read(b, len, b.length - len);
+		              if (n == -1) {
+		                  if (len < b.length) {
+		                      byte[] c = new byte[len];
+		                      System.arraycopy(b, 0, c, 0, len);
+		                      b = c;
+		                  }
+		                  return b;
+		              }
+		              len += n;
+		              if (len == b.length) {
+		                  int last = is.read();
+		                  if (last < 0) {
+		                      return b;
+		                  }
+		                  byte[] c = new byte[b.length + 1000];
+		                  System.arraycopy(b, 0, c, 0, len);
+		                  c[len++] = (byte) last;
+		                  b = c;
+		              }
+		          }
+            } finally {
+            	try {
+            		is.close ();
+            	} catch (IOException ex) {
+            		//ignore
+            	}
             }
         }
 
