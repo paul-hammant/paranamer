748447dc9ca4fadee11ca3ca86d38736087992ae
Robert Scholte <rfscholte@codehaus.org>
2014-08-16 20:57:07 +0200

    Use generics for Mojo

--- paranamer-maven-plugin/src/java/com/thoughtworks/paranamer/mojo/ParanamerGeneratorMojo.java
+++ paranamer-maven-plugin/src/java/com/thoughtworks/paranamer/mojo/ParanamerGeneratorMojo.java
@@ -30,40 +30,36 @@
 
 package com.thoughtworks.paranamer.mojo;
 
+import java.io.IOException;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
 import com.thoughtworks.paranamer.generator.ParanamerGenerator;
 import com.thoughtworks.paranamer.generator.QdoxParanamerGenerator;
-
-import java.io.IOException;
-
 
 /**
  * Mojo to generate parameter names via ParanamerGenerator
  *
  * @author Mauro Talevi
- * @goal generate
- * @phase compile
- * @requiresDependencyResolution compile
  */
+@Mojo(name="generate", defaultPhase=LifecyclePhase.COMPILE, requiresDependencyResolution=ResolutionScope.COMPILE)
 public class ParanamerGeneratorMojo extends AbstractMojo {
 
     /**
      * The directory containing the Java source files
-     *
-     * @parameter default-value="${project.build.sourceDirectory}"
-     * @required
      */
+	@Parameter(defaultValue="${project.build.sourceDirectory}", required=true)
     protected String sourceDirectory;
 
     /**
      * The directory where the Paranamer generator will write the output
-     *
-     * @parameter default-value="${project.build.outputDirectory}"
-     * @required
      */
+	@Parameter(defaultValue="${project.build.outputDirectory}", required=true)
     protected String outputDirectory;
 
     /**
@@ -71,7 +67,7 @@ public class ParanamerGeneratorMojo extends AbstractMojo {
      */
     private ParanamerGenerator generator = new QdoxParanamerGenerator();
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException {
         getLog().info("Generating parameter names from " + sourceDirectory + " to " + outputDirectory);
         try {
             generator.processSourcePath(sourceDirectory, outputDirectory);
